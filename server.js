e5ca3bda5710569b281030d8f434e899cbce594a
Jill Cunningham <jleighcunningham@gmail.com>
2022-05-05 16:20:41 -0400

    updated main server

--- server.js
+++ server.js
@@ -1,41 +1,43 @@
 //dependencies
 //var cloudinary = require('cloudinary').v2;
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
+const path = require("path");
+const express = require("express");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const session = require("express-session");
+const helpers = require("./utils/helpers");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
-app.use(session(sess));
-
-const helpers = require('./utils/helpers');
-
-const hbs = exphbs.create({ helpers });
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
 
-app.use(require('./controllers/'));
+app.use(routes);
+app.use(require("./controllers/"));
+
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+
+//const helpers = require("./utils/helpers");
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
